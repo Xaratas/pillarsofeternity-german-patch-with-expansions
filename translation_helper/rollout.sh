f17e26a5fe5069285179e307824df319656c918a
ckirschner <singu.b@gmail.com>
2015-09-02 22:17:55 +0200

    Rollout Script angepasst für neues git. Erstellt zip und 7z im translation_helper ordner.

--- translation_helper/rollout.sh
+++ translation_helper/rollout.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 CURENT_PATH=$(pwd)
-rm -rf ./../PillarsOfEternity_Data
-rm ./../latest.zip
-git checkout-index -a --prefix=../PillarsOfEternity_Data/data/localized/de-patched/
+cd translation_helper/
+rm -rf ./PillarsOfEternity_Data
+rm ./with_expansion.zip ./with_expansion.7z ./without_expansion.zip ./without_expansion.7z
 cd ..
-zip latest.zip -9 -r -q PillarsOfEternity_Data/ -x@PillarsOfEternity_Data/data/localized/de-patched/exclude.lst
+# geht nur von eins weiter oben korrekt … versteh einer git
+git checkout-index -a --prefix=translation_helper/PillarsOfEternity_Data/
+cd translation_helper/
+
+# der zip Befehl geht so nicht in bin/bash
+zip with_expansion.zip -9 -r -q PillarsOfEternity_Data/ -x @exclude.lst
+zip without_expansion.zip -9 -r -q PillarsOfEternity_Data/ -x @exclude.lst \*/data_expansion1/\*
+
+7z a -t7z -m0=lzma -mx=9 -bd -xr@exclude.lst with_expansion.7z PillarsOfEternity_Data/
+7z a -t7z -m0=lzma -mx=9 -bd -xr@exclude.lst -xr!data_expansion1\* without_expansion.7z PillarsOfEternity_Data/
+
+rm -rf ./PillarsOfEternity_Data
 cd $CURRENT_PATH
