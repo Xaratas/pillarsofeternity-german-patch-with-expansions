e331ae04336ae39309cef06577f8927e1a24fe30
xaratas <xaratas@example.com>
2015-12-03 22:10:02 +0100

    Wer braucht schon zip, wenn er 7z installiert hat? PPMD ist für Texte viel besser geeignet als lzma. ~30% zusätzliche Reduktion

--- translation_helper/rollout.sh
+++ translation_helper/rollout.sh
@@ -9,11 +9,11 @@ git checkout-index -a --prefix=translation_helper/PillarsOfEternity_Data/
 cd translation_helper/
 
 # der zip Befehl geht so nicht in bin/bash
-zip with_expansion.zip -9 -r -q PillarsOfEternity_Data/ -x @exclude.lst
-zip without_expansion.zip -9 -r -q PillarsOfEternity_Data/ -x @exclude.lst \*/data_expansion1/\*
+7z a -tzip -mm=ppmd -mmem=256m -mo=16 -mx=9 -xr@exclude.lst with_expansion.zip PillarsOfEternity_Data/
+7z a -tzip -mm=ppmd -mmem=256m -mo=16 -mx=9 -xr@exclude.lst -xr!data_expansion1\* without_expansion.zip PillarsOfEternity_Data/
 
-7z a -t7z -m0=lzma -mx=9 -bd -xr@exclude.lst with_expansion.7z PillarsOfEternity_Data/
-7z a -t7z -m0=lzma -mx=9 -bd -xr@exclude.lst -xr!data_expansion1\* without_expansion.7z PillarsOfEternity_Data/
+7z a -t7z -m0=ppmd:mem=28:o=12 -mx=9 -bd -xr@exclude.lst with_expansion.7z PillarsOfEternity_Data/
+7z a -t7z -m0=ppmd:mem=28:o=12 -mx=9 -bd -xr@exclude.lst -xr!data_expansion1\* without_expansion.7z PillarsOfEternity_Data/
 
 rm -rf ./PillarsOfEternity_Data
 cd $CURRENT_PATH
